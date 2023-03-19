08ce53e3564834361564506bdef637bc3d8aefd8
Omooba442 <74331985+Omooba442@users.noreply.github.com>
2023-03-19 16:44:11 +0100

    Update HomeFiveAbout.js

--- components/about/HomeFiveAbout.js
+++ components/about/HomeFiveAbout.js
@@ -12,9 +12,17 @@ const HomeFiveAbout = () =>{
                     </div>
                     <div>
                         <div className="uk-panel">
-                            <h3 className="uk-h3 uk-h2@s uk-heading-d2@m uk-text-uppercase">About <span className="uk-text-gradient">collection</span></h3>
-                            <p className="uk-text-large">Content creation optimized for social media engagment and tangible results.</p>
-                            <p className="uk-text-large">Social media marketing through turn key influencer marketing compaigns and social media account managment to grow your digital art and drive your collection</p>
+                            <h3 className="uk-h3 uk-h2@s uk-heading-d2@m uk-text-uppercase">Executive <span className="uk-text-gradient">Summary</span></h3>
+                            <p className="uk-text-large">What will a digitalized auto repairing and car washing service look like? It is simple; it is a system that will
+be available at the fingertip of everyone and can be easily adopted, given its digital presence.
+Our system uses blockchain technology to enhance the chance of participation and get handson
+verified
+data that allows us to optimize operations globally.</p>
+                            <p className="uk-text-large">While UpWash is currently in Finland, with topnotch
+technology and our NFTs adoption, we can understand the dire need for our service through quality data
+and ensure expansion.
+Our customer base is unique as it is obvious that we're breaking into a market of existing users with
+sufficient innovation to change the course of things and let everyone enjoy impeccable service easily.</p>
                             <div className="uk-panel uk-margin-large-top">
                                 <div className="uk-grid uk-grid-2xsmall uk-grid-small@m uk-child-width-1-2 uk-child-width-1-3@s uk-flex-middle uk-flex-center" data-uk-grid="">
                                     <div>
