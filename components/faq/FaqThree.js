07f014e02414786bad9a0b18e2c103a3bc0a050f
Omooba442 <74331985+Omooba442@users.noreply.github.com>
2023-03-19 17:15:04 +0100

    Update FaqThree.js

--- components/faq/FaqThree.js
+++ components/faq/FaqThree.js
@@ -13,33 +13,64 @@ const FaqThree = () => {
                             <div>
                                 <ul className="uk-card uk-card-small uk-card-large@m uk-radius uk-radius-large@m uk-width-2xlarge@m uk-margin-auto uk-border uk-box-shadow-xsmall dark:uk-background-white-5" data-uk-accordion="collapsible: true">
                                     <li>
-                                        <a className="uk-accordion-title uk-h5@m" href="#">What is Nerko's NFT Collection?</a>
+                                        <a className="uk-accordion-title uk-h5@m" href="#">Upwash In Metaverse?</a>
                                         <div className="uk-accordion-content uk-padding-small-bottom">
-                                            <p className="uk-text-small uk-text-large@m uk-text-muted">Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
+                                            <p className="uk-text-small uk-text-large@m uk-text-muted">UpWash is the first car wash and auto repair brand in the metaverse. We're not just going digital; we are
+exploring the virtual world like no other to provide users and avatars with an amazing world of exploration.
+Metaverse is a term that includes virtual reality and is characterized by the presence of a virtual world that
+continues to exist even when you're not actively participating. .</p>
                                         </div>
                                     </li>
                                     <li>
-                                        <a className="uk-accordion-title uk-h5@m" href="#">How we can buy and invest NFT?</a>
+                                        <a className="uk-accordion-title uk-h5@m" href="#">About Upwash In metaverse?</a>
                                         <div className="uk-accordion-content uk-padding-small-bottom">
-                                            <p className="uk-text-small uk-text-large@m uk-text-muted">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
+                                            <p className="uk-text-small uk-text-large@m uk-text-muted">It is an online, 3D universe consisting of
+multiple virtual spaces allowing users to work together, play games, or socialize with one another.
+You can order our car washing service in the metaverse by owning an avatar. You can also increase the fun
+by paying to have our mascot washers do the job in full monkey suits.
+Our Avatars provide this service in the metaverse and even allow you to choose who washes the car. We
+have 1% UpWash mascot, 20% female driver/washer, and 79% male driver/washer.</p>
                                         </div>
                                     </li>
                                     <li>
-                                        <a className="uk-accordion-title uk-h5@m" href="#">Why we should choose Nerko's NFT?</a>
+                                        <a className="uk-accordion-title uk-h5@m" href="#">Upwash's Executive Summary</a>
                                         <div className="uk-accordion-content uk-padding-small-bottom">
-                                            <p className="uk-text-small uk-text-large@m uk-text-muted">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
+                                            <p className="uk-text-small uk-text-large@m uk-text-muted">What will a digitalized auto repairing and car washing service look like? It is simple; it is a system that will
+be available at the fingertip of everyone and can be easily adopted, given its digital presence.
+Our system uses blockchain technology to enhance the chance of participation and get handson
+verified
+data that allows us to optimize operations globally.</p>
                                         </div>
                                     </li>
                                     <li>
-                                        <a className="uk-accordion-title uk-h5@m" href="#">Where we can buy and sell NFts?</a>
+                                        <a className="uk-accordion-title uk-h5@m" href="#">Our current Locations?</a>
                                         <div className="uk-accordion-content uk-padding-small-bottom">
-                                            <p className="uk-text-small uk-text-large@m uk-text-muted">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
+                                            <p className="uk-text-small uk-text-large@m uk-text-muted">While UpWash is currently in Finland, with topnotch
+technology and our NFTs adoption, we can understand the dire need for our service through quality data
+and ensure expansion.
+Our customer base is unique as it is obvious that we're breaking into a market of existing users with
+sufficient innovation to change the course of things and let everyone enjoy impeccable service easily.</p>
                                         </div>
                                     </li>
                                     <li>
-                                        <a className="uk-accordion-title uk-h5@m" href="#">How secure is this token?</a>
+                                        <a className="uk-accordion-title uk-h5@m" href="#">The project Innovation</a>
                                         <div className="uk-accordion-content uk-padding-small-bottom">
-                                            <p className="uk-text-small uk-text-large@m uk-text-muted">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
+                                            <p className="uk-text-small uk-text-large@m uk-text-muted">UpWash will digitize the global car wash and automotive repair services industry by converting them into a
+Deliveredtoyourdoor SERVICE.
+A simple look at the Blockchain industry shows the chance for enormous innovations. Our world is ready to
+embrace the new change, and with sufficient tech knowledge, UpWash will introduce a groundbreaking
+innovation that takes the car wash and car repair industry from its current state to a better position.
+Our core system dwells on the urgency of the need to repair a car. Car owners are often left in despair on
+the roadside or even a few minutes to an urgent meeting with no help. However, this does not stop here. A
+car can break down in the middle of nowhere, exposing the car owner to a significant risk that is becoming
+alarming nowadaysBy providing car owners with expedient car repair service in their location, UpWash will be helping people
+in despair by providing the service they naturally need to walk some miles or wait for hours to get.
+With UpWash, car washing or repairing services will become available on every mobile device, and with
+simple participation, our users can rest assured that help is coming even when their car breaks down in
+the middle of nowhere.
+UpWash is an extensive earning protocol for investor. We provide investor opportunities to participate
+actively in the ecosystem. Hence, our investors can engage in the brand expansion process as our
+partners and earn more money.</p>
                                         </div>
                                     </li>
                                     <li>
